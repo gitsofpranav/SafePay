70ab245042285411125be4ee9c6b4fd48fd342e6
gitsofpranav <dewanganpranav2@gmail.com>
2026-01-14 21:36:05 +0530

    Introduce MongoDB entities for reward service

---
+++ reward_service/src/main/java/com/safepay/reward_service/entity/Reward.java
@@ -0,0 +1,27 @@
+package com.safepay.reward_service.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "reward")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reward {
+    
+    @Id
+    private String id;
+    private Long userId;
+    private Double points;
+    private LocalDateTime sentAt;
+    @Indexed(unique = true)
+    private Long transactionId;
+
+}
