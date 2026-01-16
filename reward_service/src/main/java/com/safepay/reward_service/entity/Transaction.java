ccd16619c9a0943f0dbb556b408230e0b8028480
gitsofpranav <dewanganpranav2@gmail.com>
2026-01-16 19:18:49 +0530

    Introduce Transaction entity for reward service

---
+++ reward_service/src/main/java/com/safepay/reward_service/entity/Transaction.java
@@ -0,0 +1,44 @@
+package com.safepay.reward_service.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.Indexed;
+
+@Document(collection = "transactions")
+@CompoundIndex(
+    name = "sender_receiver_timestamp_idx",
+    def = "{ 'senderId': 1, 'receiverId': 1, 'timestamp': -1 }"
+)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Transaction {
+
+    @Id
+    private String id;
+
+    @Indexed
+    private Long senderId;
+
+    @Indexed
+    private Long receiverId;
+
+   
+    private Double amount;
+
+    @Indexed
+    private LocalDateTime timestamp;
+
+    @Indexed
+    private String status; 
+
+    
+}
