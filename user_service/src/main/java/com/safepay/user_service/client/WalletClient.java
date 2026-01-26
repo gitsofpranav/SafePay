515787f8263142f2e2a978e99a620e98ec5fbf47
gitsofpranav <dewanganpranav2@gmail.com>
2026-01-26 12:48:23 +0530

    Introduce wallet service and external client integration for user service

---
+++ user_service/src/main/java/com/safepay/user_service/client/WalletClient.java
@@ -0,0 +1,12 @@
+package com.safepay.user_service.client;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+// @FeignClent(name = "wallet_service", url = "http://localhost:8088/api/v1/wallets")
+// public interface WalletClient {
+
+//     @PostMapping
+//     WalletResponse createWallet(@RequestBody CreateWaletRequest request)
+    
+// } 
